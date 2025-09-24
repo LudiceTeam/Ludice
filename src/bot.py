5d034bf433d6079b38c6ac527f278afb5b5dd414
Black-Hole <110691847+DeepBlackHole@users.noreply.github.com>
2025-09-24 20:41:48 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -22,14 +22,34 @@ from os import getenv
 
 
 
-from aiogram import Bot, Dispatcher, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+import logging
 
-bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher()
+from handlers import start, help 
 
+API_TOKEN = "YOUR_TOKEN_HERE"
+
+def send_user_to_api(username, user_id):
+    url = "http://0.0.0.0:8000/register"
+    data = {
+        "username": username,
+        "id": user_id
+    }
+    resp = requests.post(url, json=data)
+    return resp.status_code == 200
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    user_id = message.from_user.id
+    username = message.from_user.username
+    if send_user_to_api(username, user_id):
+        await message.reply("You have been registered successfully!")
+    else:
+        await message.reply("Registration failed. Please try again later.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
 
 
