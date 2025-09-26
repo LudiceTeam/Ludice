9f3c62280d558f6e96cddc23a38818d0a5d1b89c
Black-Hole <110691847+DeepBlackHole@users.noreply.github.com>
2025-09-26 13:30:03 +0200

    Add rgame some changes in create_party for optimisation.

--- src/app/routers/create_party.py
+++ src/app/routers/create_party.py
@@ -1,8 +1,8 @@
 # /create_party (command handler)
 import logging
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
 from app.states import CreatePaerty
