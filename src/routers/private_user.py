0a98f51fa1b65fec4308d39fe9dbd0b7d3419ea3
Black-Hole <110691847+DeepBlackHole@users.noreply.github.com>
2025-10-05 01:12:38 +0200

    add top up

--- src/routers/private_user.py
+++ src/routers/private_user.py
@@ -1,18 +1,35 @@
 from aiogram import F,Router, types
-from aiogram.filters import CommandStart, or_f
+from aiogram.types import LabeledPrice
+from aiogram.filters import CommandStart
 
-from keyboard import reply
+from handlers.payment import tw_payment_keyboard
+from keyboard import answer
 
 start_router = Router()
-
+game = Router()
 
 @start_router.message(CommandStart())
 async def cmd_start(message: types.Message):
-    await message.answer("Welcome to the ludicé bot. Choose an option:", reply_markup=reply.start_kb)
+    await message.answer("Welcome to the ludicé bot. Choose an option:", reply_markup=answer.start_kb)
 
-@start_router.message(F.text == "Statictic 📊")
-async def about_cmd(message: types.Message):
-    await message.answer(
-        "Open source project was created by enthusiasts.\n"
-        "<a href='https://github.com/DeepBlackHole'>GitHub Repository</a>",
-        parse_mode="HTML", reply_markup=reply.del_keyboard,statictic_kb)
+@start_router.message(F.text == "Top up 🔝")
+async def stars(message: types.Message):
+    await message.answer("Choose a payment amount:", reply_markup=answer.keyboard_stars)
+
+
+@start_router.callback_query(F.data == "star15")
+async def handle_star15(callback: types.CallbackQuery):
+    prices = [LabeledPrice(label="15 ⭐", amount=20)]
+    await callback.message.answer_invoice(
+        title="❖ Top up 15⭐",
+        description="Your account will be credited with 15⭐ for 20⭐ when you complete the Telegram Stars payment.",
+        payload="topup_20",
+        provider_token="",    
+        currency="XTR",
+        prices=prices,
+        reply_markup=tw_payment_keyboard(),
+    )
+    await callback.answer() 
+    await callback.message.edit_reply_markup(reply_markup=None)
+    await callback.message.delete()
+    
