5dfc1733c0f998b1094389742e3dcb1444e750a1
Wizer27 <svd2yar@gmail.com>
2025-10-07 13:26:29 +0300

    Signature

--- src/financne.py
+++ src/financne.py
@@ -2,6 +2,24 @@ import requests as req
 import json
 from pydantic import Field,BaseModel
 from fastapi import FastAPI,HTTPException,Header
+import time 
+import hmac
+import hashlib
+
+
+KEY = ""
+def verify_signature(data: dict, received_signature: str) -> bool:
+    if time.time() - data.get('timestamp', 0) > 300:
+        return False
+    
+    
+    data_to_verify = data.copy()
+    data_to_verify.pop("signature", None)
+    
+    data_str = json.dumps(data_to_verify, sort_keys=True, separators=(',', ':'))
+    expected_signature = hmac.new(KEY.encode(), data_str.encode(), hashlib.sha256).hexdigest()
+    
+    return hmac.compare_digest(received_signature, expected_signature)
 
 
 app = FastAPI()
@@ -39,6 +57,22 @@ class TON_Payment(Payment):
             headers=headers
         )
         return response.json()
+def get_token():
+    pass
+
+def get_url():
+    pass
+
+class Pay(BaseModel):
+    username:str
+    amount:int
+    message:str
+    signature:str
+    timestamp:float = Field(default_factory=time.time)
+@app.post("/user/pay")
+async def pay(request:Pay):
+    pass
+
 
 
 
