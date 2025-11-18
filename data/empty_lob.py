52fc9ad44cd2901eef2f31bd0336ed2ad3ab7b24
Wizer27 <svd2yar@gmail.com>
2025-11-18 21:21:55 +0300

    transforming empty lobby into api

--- data/empty_lob.py
+++ data/empty_lob.py
@@ -1,11 +1,36 @@
 import argparse
 import json
 import uuid
-from fastapi import FastAPI,HTTPException,Header,Depends
+from fastapi import FastAPI,HTTPException,Header,Depends,Request
 from pydantic import Field,BaseModel
 import uuid
 import time
 import hmac
+import hashlib
+
+
+
+def get_special_key() -> str:
+    try:
+        with open("secrets.json","r") as file:
+            data  = json.load(file)
+        return data["key"]    
+    except Exception as e:
+        print(f"Error : {e}")
+        raise KeyError("Error")
+
+def verify_signature(data: dict, received_signature: str) -> bool:
+    if time.time() - data.get('timestamp', 0) > 300:
+        return False
+    KEY = get_special_key()
+    
+    data_to_verify = data.copy()
+    data_to_verify.pop("signature", None)
+    
+    data_str = json.dumps(data_to_verify, sort_keys=True, separators=(',', ':'))
+    expected_signature = hmac.new(KEY.encode(), data_str.encode(), hashlib.sha256).hexdigest()
+    
+    return hmac.compare_digest(received_signature, expected_signature)
 
 
 path = "/Users/vikrorkhanin/Ludice/data/game.json"
@@ -31,3 +56,24 @@ if args.count:
         write_empty()
 else:
     print("Wrong arguments")    
+
+def get_api_key() -> str:
+    try:
+        with open("secrets.json","r") as file:
+            data  = json.load(file)
+        return data["api"]    
+    except Exception as e:
+        print(f"Error : {e}")
+        raise KeyError("Error")
+
+async def safe_get(req:Request):
+    api = req.headers.get("X-API-KEY")
+    if not api or not hmac.compare_digest(api,get_api_key()):
+        raise HTTPException(status_code=403,detail = "Forbitten")
+
+app = FastAPI()
+
+@app.get("/")
+async def main():
+    return "API for empty lobby"
+
