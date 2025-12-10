c8390b84142e7f9037a07f44f40e025fbd8c3254
Wizer27 <svd2yar@gmail.com>
2025-12-10 10:17:11 +0300

    rewrite the start game endpoint to slq

--- backend/sql_database/game_table/game_models.py
+++ backend/sql_database/game_table/game_models.py
@@ -7,8 +7,8 @@ metadata_obj = MetaData()
 game_table = Table(
     "game_data",
     metadata_obj,
-    Column("bet",Integer,primary_key=True),
+    Column("bet",Integer),
     Column("players",ARRAY(String)),
-    Column("id",String),
+    Column("id",String,primary_key=True),
     Column("winner",String)
 )
